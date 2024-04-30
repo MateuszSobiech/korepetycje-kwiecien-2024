c02b6ccc0f80b10355ade91be6f05c395b6e4e86
Mateusz Sobiech <lordpython8@gmail.com>
2024-04-30 17:41:49 +0200

    Add ContextConsumer component

---
+++ src/HOC/ContextConsumer.tsx
@@ -0,0 +1,54 @@
+import { Dispatch, SetStateAction, createContext, useState } from 'react';
+
+const CountContext = createContext<
+  | {
+      count: number;
+      setCount: Dispatch<SetStateAction<number>>;
+    }
+  | undefined
+>(undefined);
+
+export const ContextConsumer = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <CountContext.Provider value={{ count, setCount }}>
+      <Child1 />
+    </CountContext.Provider>
+  );
+};
+
+const Child1 = () => {
+  return (
+    <>
+      <h1>Child1</h1>
+      <Child2 />
+    </>
+  );
+};
+
+const Child2 = () => {
+  return (
+    <>
+      <h2>Child2</h2>
+      <Child3 />
+    </>
+  );
+};
+
+const Child3 = () => {
+  return (
+    <CountContext.Consumer>
+      {(value) => {
+        if (!value) throw new Error('CountContext used out of context');
+
+        return (
+          <>
+            <h3>Child3: {value.count}</h3>
+            <button onClick={() => value.setCount((count) => count + 1)}>Increment</button>
+          </>
+        );
+      }}
+    </CountContext.Consumer>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,13 @@ import { RenderProps } from './Render Props/RenderProps';
 import { Optimistic } from './Optimistic ADD/Optimistic';
 import { TODOOptimistic } from './Optimistic ADD/TODOOptimistic';
 import { BindCallApply } from './bind call aplly/BindCallApply';
+import { ContextConsumer } from './HOC/ContextConsumer';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
     <Hoc name={'LIST'} />
+    {/* <HocHook name={'LIST'} /> */}
+    {/* <ContextConsumer /> */}
 
     {/* <RenderProps /> */}
 
